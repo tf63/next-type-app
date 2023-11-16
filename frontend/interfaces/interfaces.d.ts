b5c020189304a2571f1416687e08e666392315cf
tf63 <tfukuda9031@gmail.com>
2023-11-16 14:42:20 +0900

    implement select page

--- frontend/interfaces/interfaces.d.ts
+++ frontend/interfaces/interfaces.d.ts
@@ -6,3 +6,23 @@ export interface GameState {
     indexText: number
     indexLine: number
 }
+
+export interface TagAPI {
+    name: string
+}
+
+export interface LanguageAPI extends TagAPI {
+    language_id: number
+}
+
+export interface ToolAPI extends TagAPI {
+    tool_id: number
+}
+
+export interface AlgorithmAPI extends TagAPI {
+    algorithm_id: number
+}
+
+export interface PatternAPI extends TagAPI {
+    pattern_id: number
+}
--- frontend/pages/select.tsx
+++ frontend/pages/select.tsx
@@ -6,8 +6,8 @@ import SelectBoard from '@/components/SelectBoard'
 const Select: NextPage = () => {
     return (
         <main>
-            <SelectBoard />
             <LinkedButton href="/game" text="Game" color="blue" />
+            <SelectBoard />
         </main>
     )
 }
