d39d5137657113389f675906f23cc1768c7475ca
tf63 <tfukuda9031@gmail.com>
2023-11-22 12:34:40 +0900

    impl api routes

--- frontend/interfaces/interfaces.d.ts
+++ frontend/interfaces/interfaces.d.ts
@@ -1,28 +1,47 @@
 // interfaces
 
-export interface GameState {
-    typeList: string[]
-    prefixList: string[]
-    indexText: number
-    indexLine: number
+import { Category } from '@/types/types'
+
+export interface Language {
+    language_id: number
+}
+
+export interface Framework {
+    tool_id: number
+}
+
+export interface Algorithm {
+    algorithm_id: number
+}
+
+export interface Pattern {
+    pattern_id: number
 }
 
 export interface TagAPI {
     name: string
 }
 
-export interface LanguageAPI extends TagAPI {
-    language_id: number
+export interface LanguageAPIResponse extends TagAPI, Language {}
+export interface FrameworkAPIResponse extends TagAPI, Framework {}
+export interface AlgorithmAPIResponse extends TagAPI, Language, Algorithm {}
+export interface PatternAPIResponse extends TagAPI, Language, Pattern {}
+
+export interface ProblemCodeAPIRequest {
+    size: string
 }
 
-export interface ToolAPI extends TagAPI {
-    tool_id: number
+export interface LanguageCodeAPIRequest extends ProblemCodeAPIRequest, Language {}
+export interface FrameworkCodeAPIRequest extends ProblemCodeAPIRequest, Framework {}
+export interface AlgorithmCodeAPIRequest extends ProblemCodeAPIRequest, Language, Algorithm {}
+export interface PatternCodeAPIRequest extends ProblemCodeAPIRequest, Language, Pattern {}
+
+export interface ProblemCodeAPIResponse {
+    nrow: number
+    content: string
 }
 
-export interface AlgorithmAPI extends TagAPI {
-    algorithm_id: number
-}
-
-export interface PatternAPI extends TagAPI {
-    pattern_id: number
-}
+export interface LanguageCodeAPIResponse extends ProblemCodeAPIResponse, Language {}
+export interface FrameworkCodeAPIResponse extends ProblemCodeAPIResponse, Framework {}
+export interface AlgorithmCodeAPIResponse extends ProblemCodeAPIResponse, Language, Algorithm {}
+export interface PatternCodeAPIResponse extends ProblemCodeAPIResponse, Language, Pattern {}
