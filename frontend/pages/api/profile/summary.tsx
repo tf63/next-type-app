7e492a3fb5cbcd8ec6b67783775a06566239efa2
tf63 <tfukuda9031@gmail.com>
2023-12-12 14:11:08 +0900

    refactor api routes

--- frontend/pages/api/profile/summary.tsx
+++ frontend/pages/api/profile/summary.tsx
@@ -1,22 +1,19 @@
-import { ProfileMonthAPIRequest, ProfileMonthAPIResponse } from '@/interfaces/interfaces'
+import { ProfileSummaryAPIRequest, ProfileSummaryAPIResponse } from '@/interfaces/interfaces'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { supabase } from '@/lib/supabase'
 import { PostgrestError } from '@supabase/supabase-js'
 
 type ResponseObject = {
-    data: ProfileMonthAPIResponse[] | null
+    data: ProfileSummaryAPIResponse[] | null
     error: PostgrestError | null
 }
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-    const body: ProfileMonthAPIRequest = req.body
+    const body: ProfileSummaryAPIRequest = req.body
 
     const { data, error }: ResponseObject = await supabase
-        .from('user_log_miss_prev_per_type')
-        .select('month, miss_prev_per_type')
-        .order('month', { ascending: false })
-        .eq('user_id', body.userId)
-        .range(body.offset, body.offset + body.num - 1)
+        .rpc('get_user_log_summary', { user_id_input: body.userId })
+        .select(`*`)
 
     if (error != null) {
         res.status(503).json({ error: error })
